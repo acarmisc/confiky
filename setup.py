6d7ceadfd31077b22b31dc95bbe45ff1d9be9445
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2017-02-14 15:50:54 +0100

    missing version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='confiky',
-    version='0.1',    
+    version='0.1.1',    
     scripts=['confiky.py'],
     description='Read one or more .ini config file and return sections and params as object attributes',
     url='https://github.com/acarmisc/confiky',
